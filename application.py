ef9f0e8fad085b084cd930a20e0e5aafc2e7d8bf
sundar-swetha <swsundar@microsoft.com>
2020-09-04 10:16:58 +0800

    Minor Bug: Fix typo in the application file

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Merlion Crew!"
 
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
